33d78d8bcb3e6818fa5ca9c90a22b973bff8b83c
Sergey <68071039+mysteryon88@users.noreply.github.com>
2022-11-12 19:13:14 +0300

    delete old readme

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -7,19 +7,11 @@ async function main() {
   await authentication.deployed();
   console.log(`Authentication deployed to ${authentication.address}`);
 
-  const [, hacker] = await ethers.getSigners();
-  
-  console.log(await authentication.connect(hacker).getOwner());
-
   console.log(
     "owner =", 
     await ethers.provider.getStorageAt(authentication.address, 0)
   );
 
-  console.log(
-    "a =", 
-    await ethers.provider.getStorageAt(authentication.address, 2)
-  );
 } 
 
 main().catch((error) => {
